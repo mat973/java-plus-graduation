67778f0b0a86912d849766cb91a005d030b7c561
Matvey <matvey.oleynik.2017@mail.ru>
2025-10-14 21:14:21 +0300

    fix some property trouble

--- core/main-service/src/main/java/ru/practicum/MainService.java
+++ core/main-service/src/main/java/ru/practicum/MainService.java
@@ -2,9 +2,8 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("ru.practicum")
+
 @SpringBootApplication
 public class MainService {
     public static void main(String[] args) {
